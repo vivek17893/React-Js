4a0ede3a37b1a7e3b03ed14e4081936cd37320b8
vivek17893 <vivekkv178@gmail.com>
2018-05-05 07:49:25 +0530

    Added reduce function for dynamic content

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,13 +7,22 @@ import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 // <BurgerIngredient type="cheese" />
 // <BurgerIngredient type="meat" />
 
+// return [
+//   [<p>1</p>, <p>2</p>],
+//   [<p>3</p>, <p>4</p>],
+// ];
+
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-    // return <BurgerIngredient key={igKey + 1} type={igKey} />;
-  });
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+      // return <BurgerIngredient key={igKey + 1} type={igKey} />;
+    })
+    .reduce((arr, element) => {
+      return arr.concat(element);
+    }, []);
   console.log(transformedIngredients);
   return (
     <div className="Burger">
