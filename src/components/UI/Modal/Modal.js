ca8a428d963864fe153906170fbe043654ae778b
vivek17893 <vivekkv178@gmail.com>
2018-05-06 11:32:42 +0530

    Performance improved in Modal.js

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Modal.css";
 import Aux from "../../../hoc/Aux";
 import Backdrop from "../Backdrop/Backdrop";
-const modal = props => {
-  return (
-    <Aux>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div
-        className="Modal"
-        style={{
-          transform: props.show > 0 ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show > 0 ? "1" : "0"
-        }}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className="Modal"
+          style={{
+            transform:
+              this.props.show > 0 ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show > 0 ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
-export default modal;
+export default Modal;
+
+// const modal = props => {
+//   return (
+//     <Aux>
+//       <Backdrop show={props.show} clicked={props.modalClosed} />
+//       <div
+//         className="Modal"
+//         style={{
+//           transform: props.show > 0 ? "translateY(0)" : "translateY(-100vh)",
+//           opacity: props.show > 0 ? "1" : "0"
+//         }}
+//       >
+//         {props.children}
+//       </div>
+//     </Aux>
+//   );
+// };
+
+// export default modal;
